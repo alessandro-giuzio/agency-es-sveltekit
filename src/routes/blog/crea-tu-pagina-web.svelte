4d5f6b89aa1ca8538148d685e820a7786d87c363
alessandro-giuzio <giuzio@icloud.com>
2022-04-02 06:40:48 +0200

    sitios web

--- src/routes/blog/crea-tu-pagina-web.svelte
+++ src/routes/blog/crea-tu-pagina-web.svelte
@@ -11,12 +11,39 @@
     <h3 class="text-balihali font-bold uppercase font-gothic text-l text-center mb-12">Last update 03 Jan 2022 </h3>
 </div>
 </main>
-<div class="grid grid-cols-12">
+<div class="grid grid-cols-12 gap-6">
 
  <div class="col-span-12 md:col-start-4 md:col-span-6 md:px-0">
-     <p>Conectar con sus clientes en línea es una de las mejores maneras no solo de estar presente en su mente, sino también de realizar transacciones comerciales. Una presencia en línea eficaz puede ayudarlo a crecer, y desarrollarla puede ser una empresa rentable por sí misma.</p>
+     <p>Conectar con sus clientes en línea es una de las mejores maneras no solo de estar presente en su mente, sino también de realizar transacciones comerciales. <br>  Una presencia en línea eficaz puede ayudarlo a crecer, y desarrollarla puede ser una empresa rentable por sí misma.</p>
+     <hr class="border-2 border-balihali my-8 max-w-md mx-auto">
+ </div>
+ <div class="col-span-12 md:col-start-4 md:col-span-6 md:px-0">
+    <p>Cuando su negocio esté listo para un sitio web robusto y totalmente funcional, <strong>¡póngase en manos de AgencyGA.com!</strong> Hemos diseñado varios sitios web para pequeñas empresas. Nuestra percepción estética y nuestra visión detallista prepararán su conexión digital para el éxito.</p>
+    <hr class="border-2 border-balihali my-8 max-w-md mx-auto">
+ </div>
+ <div class="col-span-12 md:col-start-4 md:col-span-6 md:px-0">
+    <p>La construcción de una presencia de negocios en línea es una excelente manera de llegar a una amplia audiencia, pero su creación puede ser un desafío desalentador. En AgencyGA, nos especializamos en el diseño de sitios web asequibles. Nos enorgullece asegurar que su punto de partida para la construcción de su presencia en la web será simple y elegante. Nuestra visión de las soluciones digitales se combina con nuestro compromiso con el diseño y la experiencia del usuario, proporcionándole la mejor calidad para su inversión.</p>
+    <hr class="border-2 border-balihali my-8 max-w-md mx-auto">
+ </div>
+ <div class="col-span-12 md:col-start-4 md:col-span-6 md:px-0">
+    <p>Internet es uno de los mejores recursos que las empresas tienen a su disposición. Las organizaciones pueden generar clientes potenciales y aumentar las ventas si tienen una amplia presencia en línea que cubra todas las plataformas sociales importantes y destaque la información clave sobre una marca, su equipo y lo que la diferencia de la competencia. Entendemos que esté ocupado y que su sitio web no sea una prioridad. Pero tener una presencia en línea que funcione es realmente importante para el éxito de su negocio. Es lo que lo separará de la competencia.</p>
+    <hr class="border-2 border-balihali my-8 max-w-md mx-auto">
+ </div>
+ <div class="col-span-12 md:col-start-4 md:col-span-6 md:px-0">
+    <p>AgencyGA es su compañero de viaje para el diseño web. Si usted está buscando avanzar en su sitio web ya existente o crear uno nuevo desde cero, nuestro equipo creativo le ayudará a alcanzar sus objetivos. Nuestros servicios aseguran una entrega rápida y cercana, además de una asistencia personal que no tiene precio.</p>
+    <hr class="border-2 border-balihali my-8 max-w-md mx-auto">
  </div>
 
+ <div class="col-span-12 md:col-start-4 md:col-span-6 md:px-0">
+     <p class="text-xl lg:text-2xl mt-12 text-gray-800 font-semibold" >
+         Hoy en día, trabajo con ciertas empresas selectas que buscan impulsar su presencia en la red para llegar a lo más alto y nunca bajar de la cima. ¿Usted también busca lo mismo?
+     </p>
+     <a href="/contacto" class="font-bold">
+         <h2 class="highlight text-3xl hover:opacity-75">
+             ¡Pónte en contacto conmigo!
+         </h2>
+     </a>
+ </div>
 </div>
 
 
